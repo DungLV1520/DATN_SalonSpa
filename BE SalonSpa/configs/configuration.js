21d028fdf16784ca7c72324a9650a34de9b1a6d3
Lường Dũng <83817925+DungLV1520@users.noreply.github.com>
2023-05-24 18:08:10 +0700

    remove key secret

--- BE SalonSpa/configs/configuration.js
+++ BE SalonSpa/configs/configuration.js
@@ -10,7 +10,4 @@ module.exports = {
     role: "ADMIN",
     status: "Activated",
   },
-  MAIL: "dunglv1520@gmail.com",
-  SENDGRID_API_KEY:
-    "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",
 };
